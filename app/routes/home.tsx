38225a41054af79c77748b11def4be6a46c77141
jitbjony <jitbjony@gmail.com>
2025-08-03 23:59:23 +0600

    Header section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import { Navbar } from "~/components/shared/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import { ResumeCards } from "~/components/cards/ResumeCards";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,10 +15,21 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track your Application Status & Ratings</h1>
           <h2>Check AI powered feedback for your dream job!</h2>
         </div>
+
+        {
+          /* This is where the ResumeCards component will be used */
+          resumes.length > 0 && (
+            <div className="resumes-section">
+              {resumes.map((resume: Resume) => (
+                <ResumeCards key={resume.id} resume={resume} />
+              ))}
+            </div>
+          )
+        }
       </section>
     </main>
   );
