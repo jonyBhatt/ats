a0d00a74ff1207cb1ddfd9df9d92424770bb8244
jitbjony <jitbjony@gmail.com>
2025-08-03 20:59:06 +0600

    Project Init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
+import { Navbar } from "~/components/shared/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ATS" },
+    { name: "description", content: "Feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Application Status & Ratings</h1>
+          <h2>Check AI powered feedback for your dream job!</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
