16582f768630b9db7c1598cc025bfbb2aeb0a9d6
jitbjony <jitbjony@gmail.com>
2025-08-03 23:58:32 +0600

    Navbar

---
+++ app/components/shared/Navbar.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="">
+        <p className="text-2xl font-bold text-gradient">ATS</p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume
+      </Link>
+    </nav>
+  );
+};
