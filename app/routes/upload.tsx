908f795c2228d8daaf525b2fae097beb1bf39a3e
jitbjony <jitbjony@gmail.com>
2025-08-05 01:40:37 +0600

    Pdf to image and AI response done

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,17 +1,98 @@
+import { prepareInstructions } from "../../constants";
 import { useRef, useState } from "react";
+import { useNavigate } from "react-router";
 import { FileUpload } from "~/components/FileUpload";
 import { Navbar } from "~/components/shared/Navbar";
+import { usePuterStore } from "~/lib/puter";
+import { convertPdfToImage } from "~/utils/pdfToImage";
+import { generateUUID } from "~/utils/UUID";
+
+interface AnalyzeProps {
+  companyName: string;
+  jobTitle: string;
+  jobDescription: string;
+  file: File;
+}
 
 export default function Upload() {
+  const navigate = useNavigate();
   const [isProcessing, setIsProcessing] = useState(false);
   const [statusText, setStatusText] = useState("");
   const [file, setFile] = useState<File | null>(null);
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState("");
+
+  const { auth, isLoading, fs, ai, kv } = usePuterStore();
 
   const handleFileSelect = (file: File | null) => {
-    console.log("File: ", file);
-
     setFile(file);
+  };
+
+  const handleAnalyze = async ({
+    companyName,
+    file,
+    jobDescription,
+    jobTitle,
+  }: AnalyzeProps) => {
+    setIsProcessing(true);
+    setStatusText("Uploading your file....");
+
+    const uploadFile = await window.puter.fs.upload([file]);
+
+    if (!uploadFile) return setStatusText("Error: Failed to upload file");
+
+    setStatusText("Converting to image...");
+    const imageFile = await convertPdfToImage(file);
+    console.log("Image File: ", imageFile);
+
+    if (!imageFile.file)
+      return setStatusText("Error: Failed to convert PDF to image");
+
+    setStatusText("Uploading...");
+    const uploadImage = await fs.upload([imageFile.file]);
+    if (!uploadImage) return setStatusText("Error: Failed to upload image");
+
+    setStatusText("Prepare data...");
+
+    const uuid = generateUUID();
+    const data = {
+      id: uuid,
+      resumePath: uploadFile.path,
+      imagePath: uploadImage.path,
+      companyName,
+      jobTitle,
+      jobDescription,
+      feedback: "",
+    };
+
+    await kv.set(`resume${uuid}`, JSON.stringify(data));
+    setStatusText("Analyzing data...");
+
+    const feedback = await ai.feedback(
+      uploadFile.path,
+      prepareInstructions({ jobTitle, jobDescription })
+    );
+
+    if (!feedback) {
+      setStatusText("Error: Failed to Analyze");
+    }
+
+    const feedbackText =
+      typeof feedback?.message.content === "string"
+        ? feedback.message.content
+        : feedback?.message.content[0].text;
+
+    data.feedback = JSON.parse(feedbackText);
+    await kv.set(`resume${uuid}`, JSON.stringify(data));
+
+    setStatusText("Analyzing complete...");
+    setImage(data.imagePath);
+    console.log("Data: ", data);
+
+    // Wait a moment to show the completed image
+    setTimeout(() => {
+      setIsProcessing(false);
+      // navigate(`/results/${uuid}`);
+    }, 2000);
   };
 
   const handleUpload = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -23,17 +104,17 @@ export default function Upload() {
     }
     const formData = new FormData(form);
 
-    const companyName = formData.get("company-name");
-    const jobTitle = formData.get("job-title");
-    const jobDescription = formData.get("job-description");
+    const companyName = formData.get("company-name") as string;
+    const jobTitle = formData.get("job-title") as string;
+    const jobDescription = formData.get("job-description") as string;
 
-    console.log(
-      `Company Name: ${companyName}, Job Title: ${jobTitle}, Job Description: ${jobDescription}`
-    );
+    if (!file) return null;
+
+    handleAnalyze({ companyName, file, jobDescription, jobTitle });
   };
 
   return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <main className="">
       <Navbar />
       <section className="main-section">
         <div className="page-heading py-10">
@@ -44,7 +125,11 @@ export default function Upload() {
               <h2>{statusText}</h2>
               <picture>
                 <img
-                  src="/images/resume-scan.gif"
+                  src={
+                    statusText === "Analyzing complete..."
+                      ? image
+                      : "/images/resume-scan.gif"
+                  }
                   alt="resume"
                   className="w-full"
                 />
